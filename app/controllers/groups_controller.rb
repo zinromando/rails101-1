7b3cac1fab6cb009be5ab40ed9b0e59a8e3a2916
zin <romandozin@gmail.com>
2016-12-29 18:27:49 +0100

    implement my groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -46,7 +46,7 @@ class GroupsController < ApplicationController
   def join
     @group = Group.find(params[:id])
 
-    if !current_user.is_member_of?(group)
+    if !current_user.is_member_of?(@group)
       current_user.join!(@group)
       flash[:notice] = '加入本讨论版成功！'
     else
